0336893769d72ac591ec04dd745cb3328c0b8e12
Anja Schaffhirt <anja@sedna-soft.de>
2023-11-21 12:58:52 +0000

    Fix copy error

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -71,7 +71,7 @@ const badgeStyle = {
   borderRadius: 4,
   padding: "4px 6px",
   display: "inline-block",
-  position: "relative" as "relative",
+  position: "relative",
   top: -2,
   marginLeft: 10,
   lineHeight: 1,
