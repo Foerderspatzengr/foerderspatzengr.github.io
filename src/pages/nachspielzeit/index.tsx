dcf21e5e3164117aed9e704f2740d36688f57696
sesogr <github-sesogr@sedna-soft.de>
2024-03-22 15:23:36 +0000

    Add next Nachspielzeit

--- src/pages/nachspielzeit/index.tsx
+++ src/pages/nachspielzeit/index.tsx
@@ -23,7 +23,15 @@ const afterHoursPage = ({data}) => {
                 <p>Das Kinderhaus schließt auch an einem solchen Tag regulär um 16:30 Uhr. Daher ist nach Ende der
                     Veranstaltung der Aufenthalt in der Kita nicht mehr gestattet und das Gebäude ist auf direktem Wege
                     zu verlassen, damit niemand versehentlich eingeschlossen wird.</p>
-                <h2>Dienstag, 2024-02-06: „Wir verzaubern das neue Jahr“</h2>
+                <h2>Mittwoch, 2024-04-17: „Es blubbert in den Birken“</h2>
+                <ul>
+                    <li>von 15:30 Uhr</li>
+                    <li>bis 16:30 Uhr</li>
+                    <li>am Schachfeld im Birkenwäldchen</li>
+                    <li><a href="/20240417-nachspielzeit-2.pdf">Plakat als PDF</a></li>
+                </ul>
+                <h2>Vergangene Veranstaltungen</h2>
+                <h3>Dienstag, 2024-02-06: „Wir verzaubern das neue Jahr“</h3>
                 <p>… mit Magier <a href="https://www.zauberstudio-kittlitz.de/">Peter Schulz</a> aus Kittlitz.</p>
                 <ul>
                     <li>15:15 Uhr Einlass</li>
@@ -42,7 +50,7 @@ const afterHoursPage = ({data}) => {
 }
 export const titlequery = graphql`
   	query titlequery {
-  		file(relativePath: {eq: "magic-show.jpg"}) {
+  		file(relativePath: {eq: "betula-bubbles.png"}) {
     		childImageSharp {
       			gatsbyImageData
     		}
