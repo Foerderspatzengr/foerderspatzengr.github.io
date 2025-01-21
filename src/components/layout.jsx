29c10ea79bc8251ae48b163c51a204b4c85e4afd
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-01-21 12:20:34 +0100

    refactor layout and styling for improved structure and readability

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,102 +2,38 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 import Fiddle from './Fiddle'
 import './global.css'
+
 const pageStyles = {
-  color: "#232129",
-  padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
+
+
+const navStyles = {
+  
+    float: "right",
+    paddingRight: "15%",
+    paddingLeft: "100%",
+    
 }
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-    display: "flex",
-    listStyle: "none",
-    paddingLeft: "0.5rem",
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
+
+const ItemStyles = {
   fontWeight: 300,
   fontSize: 24,
   maxWidth: 560,
   marginBottom: 30,
-    paddingRight: "1rem",
+  paddingRight: "1rem",
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const blogStyles = {
-    kinderhaus: {
-
-    },
-}
-
-const Layout = (class, {pageTitle, slug, children }) => {
+const Layout = ({className, pageTitle, slug, children }) => {
     return (
         
-        <div className={class} style={pageStyles}>
+        <div className={className} style={pageStyles}>
           <Fiddle />
-            <nav>
-                <ul style={listStyles}>
-                    <li style={listItemStyles}><Link to="/">Startseite</Link></li>
-                    <li style={listItemStyles}><Link to="/nachspielzeit">Nachspielzeit</Link></li>
-                    <li style={listItemStyles}><Link to="/contact">Kontakt</Link></li>
-                    <li style={listItemStyles}><Link to="/datenschutz">Datenschutzerklärung</Link></li>
-                </ul>
+            <nav style={navStyles}>
+                <Link style={ItemStyles} to="/">Startseite</Link>
+                <Link style={ItemStyles} to="/nachspielzeit">Nachspielzeit</Link>
+                <Link style={ItemStyles} to="/contact">Kontakt</Link>
+                <Link style={ItemStyles} to="/datenschutz">Datenschutzerklärung</Link>
             </nav>
             <main className={slug}>
             <h1>{pageTitle}</h1>
--- src/pages/datenschutz.tsx
+++ src/pages/datenschutz.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 import '../components/global.css'
 const datenSchutz = () => {
     return (
-        <Layout class = "datenschutz">
+        <Layout className = "datenschutz">
             <h1>Datenschutz&shy;erkl&auml;rung</h1><h2>1. Datenschutz auf einen Blick</h2><h3>Allgemeine Hinweise</h3>
             <p>Die folgenden Hinweise geben einen einfachen &Uuml;berblick dar&uuml;ber,
                 was mit Ihren personenbezogenen Daten passiert, wenn Sie diese Website besuchen. Personenbezogene Daten
